c52d7fca1003411fe84b8d1f366364c58c4b909c
mugi-uno <mugi.uno@gmail.com>
2019-03-03 15:22:21 +0900

    4章 コードを分割する

--- js/script.js
+++ js/script.js
@@ -1,49 +1,34 @@
 import $ from 'jquery';
+import { readData } from './reader';
+import {
+  writeNextTodo,
+  writeTodoCount,
+  toggleTodoList,
+  toggleTodoEmpty,
+  addTodo,
+  removeTodo
+} from './writer';
 
 function updateAll() {
-  var count = $('.todo').length;
-  var next = $('.todo input').first();
-  var nextTodoText = count ? next.val() : '(未登録)'
+  const { count, nextTodoText } = readData();
 
-  $('#nextTodo').text('次のTODO: ' + nextTodoText);
-  $('#todoCount').text('(全' + count + '件)');
-
-  if (count) {
-    $('#todoList').show();
-    $('#todoEmpty').hide();
-  } else {
-    $('#todoList').hide();
-    $('#todoEmpty').show();
-  }
+  writeNextTodo(nextTodoText);
+  writeTodoCount(count);
+  toggleTodoList(count);
+  toggleTodoEmpty(count);
 }
-
-function addTodo() {
-  var wrapper = $('<div>');
-  wrapper.addClass('todo');
-
-  var input = $('<input>');
-  input.attr('type', 'text');
-
-  var deleteButton = $('<button>');
-  deleteButton.addClass('delete').text('削除');
-
-  wrapper.append(input);
-  wrapper.append(deleteButton);
-  $('#todoList').append(wrapper);
-}
-
-$(function() {
-  $('#addTodo').on('click', function() {
+$(function () {
+  $('#addTodo').on('click', function () {
     addTodo();
     updateAll();
   });
 
-  $('#todoList').on('input', '.todo:eq(0)', function() {
+  $('#todoList').on('input', '.todo:eq(0)', function () {
     updateAll();
   });
 
-  $('#todoList').on('click', '.delete', function() {
-    $(this).closest('.todo').remove();
+  $('#todoList').on('click', '.delete', function () {
+    removeTodo(this);
     updateAll();
   });
 
